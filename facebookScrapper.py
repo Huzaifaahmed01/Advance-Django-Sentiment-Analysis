8a464656d43947d42f7f0860285dfa78156296d2
toobaamir8110 <toobaamifazal@gmil.com>
2020-12-05 10:38:32 -0800

    csv file

--- facebookScrapper.py
+++ facebookScrapper.py
@@ -1,4 +1,15 @@
 from facebook_scraper import get_posts
+import csv
+result = {}
 
-for post in get_posts('ImranKhanOfficial', pages=2):
-    print(post['text'])
+with open(' facebookScrapper.py', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for post in get_posts('ImranKhanOfficial', pages=1):
+        for row in result.items():
+            csvwriter.writerow(row)
+            if row[0] in result:
+                result[row[0]].append(row[1])
+            else:
+                result[row[0]] = [row[1]]
+        #print(result)
+        print(post['text'])    
