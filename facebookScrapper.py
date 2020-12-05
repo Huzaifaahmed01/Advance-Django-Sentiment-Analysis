a3d96a301ce5a81200123b55ff593316cb7da8e0
Huzaifa Ahmed <ha300101@hotmail.com>
2020-12-05 23:52:05 +0500

    Settled CSV Writting Code

--- facebookScrapper.py
+++ facebookScrapper.py
@@ -1,15 +1,10 @@
 from facebook_scraper import get_posts
 import csv
-result = {}
 
-with open(' facebookScrapper.py', 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
+with open('facebookScrapper.csv', 'a+') as csvfile:
+    csvwriter = csv.DictWriter(csvfile, fieldnames=['POSTS'])
+    csvwriter.writeheader()
+    result={'POSTS':''}
     for post in get_posts('ImranKhanOfficial', pages=1):
-        for row in result.items():
-            csvwriter.writerow(row)
-            if row[0] in result:
-                result[row[0]].append(row[1])
-            else:
-                result[row[0]] = [row[1]]
-        #print(result)
-        print(post['text'])    
+        result['POSTS'] = post['text']+'\n'
+        csvwriter.writerow(result)
