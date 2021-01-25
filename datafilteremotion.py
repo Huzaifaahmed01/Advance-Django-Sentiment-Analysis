16e3a45f9e09bf7fd789a7fa65b65169809e47e3
Huzaifa Ahmed <ha300101@hotmail.com>
2021-01-25 16:26:09 +0500

    DataSet Settle

---
+++ datafilteremotion.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("dataset_emotion.csv")
+dfFinal = pd.DataFrame(columns=["text","react"])
+
+df_react = df[['react_angry','react_haha','react_love','react_sad','react_wow']]
+df_react = df_react.values.tolist()
+
+dfFinal.loc[:, 'text'] = df['message']
+
+def maxReact(x):
+    dataEmotion = ['angry','joyful','lovely','sorrowful','surprising']
+    a = 0
+    for i in x:
+        if i > a:
+            a = i
+    return dataEmotion[x.index(a)]
+
+listEmotions = []
+for x in df_react:
+    abc = maxReact(x)
+    listEmotions.append(abc)
+
+reactArray = np.array(listEmotions)
+
+
+dfFinal.loc[:, 'react'] = reactArray
+
+dfFinal.to_csv('datasetFinal.csv', index=False)
