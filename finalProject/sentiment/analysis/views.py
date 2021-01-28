4b9fe352bdcc6afaddea0ea2aa4f873476a24053
Huzaifa Ahmed <ha300101@hotmail.com>
2021-01-28 15:24:02 +0500

    DataBase Model Created

--- finalProject/sentiment/analysis/views.py
+++ finalProject/sentiment/analysis/views.py
@@ -6,6 +6,7 @@ from collections import Counter
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pickle
 
 class RNN_LSTM(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_size, num_layers, num_classes, drop_prob=0.2):
@@ -23,14 +24,9 @@ class RNN_LSTM(nn.Module):
         x = x.long()
         embeds = self.embedding(x)
 
-        if (train_on_gpu):
-          # Set initial hidden and cell states
-          h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
-          c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
-        else:
-          # Set initial hidden and cell states
-          h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-          c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        # Set initial hidden and cell states
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
 
         # Forward propagate LSTM
         out, _ = self.lstm(embeds, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
@@ -42,6 +38,9 @@ class RNN_LSTM(nn.Module):
         return out
 
 def tokenize_review(test_review, fileVocab):
+    a_file = open(fileVocab, "rb")
+    vocab_to_int = pickle.load(a_file)
+
     test_review = test_review.lower() # lowercase
     # get rid of punctuation
     test_text = ''.join([c for c in test_review if c not in punctuation])
@@ -55,7 +54,7 @@ def tokenize_review(test_review, fileVocab):
 
     return test_ints
 
-def pad_features(reviews_ints, seq_length):
+def pad_features(reviews_ints, seq_length=50):
 
     # getting the correct rows x cols shape
     features = np.zeros((len(reviews_ints), seq_length), dtype=int)
@@ -66,4 +65,53 @@ def pad_features(reviews_ints, seq_length):
 
     return features
 
+def predict(net, test_review, sequence_length=50):
+    net.eval()
+
+    test_ints = tokenize_review(test_review)
+
+    seq_length=sequence_length
+    features = pad_features(test_ints, seq_length)
+
+    feature_tensor = torch.from_numpy(features)
+
+    if(train_on_gpu):
+        feature_tensor = feature_tensor.cuda()
+
+    scores = net(feature_tensor)
+    _, predictions = scores.max(1)
+
+    return scores.cpu().detach().numpy()[0], predictions
+
+def modelEmotion():
+    a_file = open('emotion.pkl', "rb")
+    vocab_to_int = pickle.load(a_file)
+
+    vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding + our word tokens
+    output_size = 13
+    embedding_dim = 400
+    hidden_dim = 256
+    n_layers = 2
+
+    net = RNN_LSTM(vocab_size, embedding_dim, hidden_dim, n_layers, output_size)
+
+    net.load_state_dict(torch.load('emotion_netFinal.pt'))
+
+    return net
+
+def modelPosNeg():
+    a_file = open('pos_neg.pkl', "rb")
+    vocab_to_int = pickle.load(a_file)
+
+    vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding + our word tokens
+    output_size = 13
+    embedding_dim = 400
+    hidden_dim = 256
+    n_layers = 2
+
+    net = RNN_LSTM(vocab_size, embedding_dim, hidden_dim, n_layers, output_size)
+
+    net.load_state_dict(torch.load('pos_neg_netFinal.pt'))
+
+    return net
 # Create your views here.
